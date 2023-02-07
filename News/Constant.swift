41ef5a37100de903933a6c7258cf63a292a43ff7
KhinPhone060 <60284009+KhinPhone060@users.noreply.github.com>
2023-02-07 23:05:46 +0630

    add bookmark feature for breaking news

---
+++ News/Constant.swift
@@ -0,0 +1,31 @@
+//
+//  Constant.swift
+//  News
+//
+//  Created by Khin Phone Ei on 07/02/2023.
+//
+
+struct Constant {
+//    static let cellIdentifier = "ReusableCell"
+//    static let cellNibName = "MessageCell"
+//    static let registerSegue = "RegisterToChat"
+//    static let loginSegue = "LoginToChat"
+//    static let appName = "⚡️FlashChat"
+    
+struct BrandColors {
+//        static let purple = "BrandPurple"
+//        static let lightPurple = "BrandLightPurple"
+//        static let blue = "BrandBlue"
+//        static let lighBlue = "BrandLightBlue"
+//        static let brandPurple = "BrandPurple"
+}
+    
+struct FStore {
+    static let collectionName = "bookmark"
+    static let user = "user"
+    static let newsURL = "url"
+    static let dateField = "date"
+    static let isBookmarked = "isBookmarked"
+    }
+}
+
--- News/Controller/BreakingNewsDetailViewController.swift
+++ News/Controller/BreakingNewsDetailViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 import ReadabilityKit
 import FirebaseAuth
+import FirebaseFirestore
+import Toast
 
 class BreakingNewsDetailViewController: UIViewController {
     
@@ -16,13 +18,17 @@ class BreakingNewsDetailViewController: UIViewController {
     @IBOutlet weak var newsTitle: UILabel!
     @IBOutlet weak var datePublished: UILabel!
     @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var saveBtn: UIButton!
     
     var news: BreakingNews?
+    
+    let db = Firestore.firestore()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         parseContent()
+        loadBookmarkIcon()
         
         let url = URL(string: news?.imageURL ?? "")
         newsImage.kf.setImage(with: url)
@@ -41,13 +47,70 @@ class BreakingNewsDetailViewController: UIViewController {
     
     @IBAction func savePressed(_ sender: UIButton) {
         if Auth.auth().currentUser?.uid != nil{
-            
-            //show toast or message that the news has been bookmarked
+                        
+            db.collection("bookmark").whereField("url", isEqualTo: news?.url)
+                .getDocuments() { (querySnapshot, err) in
+                    if let err = err {
+                        print("Error getting documents: \(err)")
+                    } else if querySnapshot!.documents.count == 0 {
+                        
+                        //when url is new url added to the firestore
+                        if let url = self.news?.url, let user = Auth.auth().currentUser?.email {
+                            self.db.collection(Constant.FStore.collectionName).addDocument(data: [
+                                Constant.FStore.user: user,
+                                Constant.FStore.newsURL: url,
+                                Constant.FStore.dateField: Date().timeIntervalSince1970
+                            ]) { (error) in
+                                if let e = error {
+                                    print("There was an issue saving data to the firestore, \(e)")
+                                } else{
+                                    self.saveBtn.setImage(UIImage(systemName: "bookmark.fill"), for: .normal)
+                                    self.view.makeToast("Saved to bookmark list")
+                                }
+                            }
+                        }
+                    } else {
+                        //remove news from bookmark
+                        self.db.collection("bookmark")
+                            .whereField("url", isEqualTo: self.news?.url)
+                            .getDocuments() { (querySnapshot, err) in
+                                if let e = err {
+                                    print("There was problem retrieving data from the firestore \(e)")
+                                } else {
+                                    for doc in querySnapshot!.documents {
+                                        doc.reference.delete()
+                                        self.saveBtn.setImage(UIImage(systemName: "bookmark"), for: .normal)
+                                        self.view.makeToast("Removed from bookmark list")
+                                        
+                                    }
+                                }
+                        }
+
+                    }
+            }
             
         } else {
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
                 let loginNavController = storyboard.instantiateViewController(identifier: "LoginNavigationController")
             (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(loginNavController)
+        }
+    }
+    
+    func loadBookmarkIcon() {
+        db.collection("bookmark")
+            .whereField("url", isEqualTo: news?.url)
+            .getDocuments() { querySnapshot, error in
+            
+            if let e = error {
+                print("There was problem retrieving data from the firestore \(e)")
+            } else if querySnapshot!.documents.count == 0 {
+                print("No documents")
+            } else {
+                
+                DispatchQueue.main.async {
+                    self.saveBtn.setImage(UIImage(systemName: "bookmark.fill"), for: .normal)
+                }
+            }
         }
     }
     
